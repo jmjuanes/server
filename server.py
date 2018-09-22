48071b2a570ef7b9f7520642e04457a1f0f629b1
jmjuanes <josemijuanes@gmail.com>
2018-09-22 23:02:45 +0200

    Rewrite parsing file method. Updated configuration keys.

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import webapp2
 import cloudstorage as gcs
 
 # Import configuration
-with open("config.json") as f:
+with open(os.path.join(os.path.dirname(__file__), "config.json")) as f:
     config = json.load(f)
 
 # Render an error message
@@ -26,15 +26,16 @@ def render_static(self, code, file_name):
 
 # Parse a path and get the file object information
 def parse_file(request):
-    file_object = {}
-    file_object["content"] = ""
-    file_object["storage_path"] = ""
-    file_object["extname"] = os.path.splitext(request.path)[1]
-    # Get the real file pathname
+    file_object = {
+        "content": "",
+        "storage_path": "",
+        "extname": ops.path.splitext(request.path)[1],
+        "pathname": request.path
+    }
+    # Check if path is a folder --> if yes, add the entry file path
     if file_object["extname"] == "":
-        file_object["pathname"] = os.path.join(request.path, config["entry_file"])
-    else:
-        file_object["pathname"] = request.path
+        file_object["pathname"] = os.path.join(request.path, config["paths"]["entry"])
+        file_object["extname"] = os.path.splitext(config["path"]["entry"])[1]
     # Get the file mime type
     file_object["mime_type"] = get_mimetype(file_object["pathname"])
     return file_object
@@ -56,36 +57,35 @@ class MainHandler(webapp2.RequestHandler):
         if self.request.host in config["redirects"]:
             return self.redirect(config["redirects"][sef.request.host])
         # Check for ignored path
-        if self.request.path in config["ignore_paths"]:
+        if self.request.path in config["paths"]["ignore"]:
             return render_error(self, 404, "Not found")
         # Get the file object
         file_object = parse_file(self.request)
+        # Get the GCS file path
+        # It has the structure "/<BUCKET>/<SUBDOMAIN>/<FILE_PATH>"
+        subdomain = get_subdomain(self.request)
+        file_object["storage_path"] = "/" + config["bucket"] + "/" + subdomain + file_object["pathname"]
         try:
-            # Get the GCS file path
-            subdomain = get_subdomain(self.request)
-            file_object["storage_path"] = "/" + config["bucket_name"] + "/" + subdomain + file_object["pathname"]
-            # logging.warning("Reading file " + file_object["storage_path"])
             # Open the file from cloud storage
+            # logging.warning("Reading file " + file_object["storage_path"])
             gcs_file = gcs.open(file_object["storage_path"], mode="r")
             file_object["content"] = gcs_file.read()
             gcs_file.close()
-            # Check for not HTML filei to add the cache header
+            # Check for not HTML file to add the cache header
             # https://cloud.google.com/appengine/docs/standard/python/config/appref#static_cache_expiration
             # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching
-            if file_object["extname"] != ".html":
-                # Save on cache for 1 hour
-                self.response.headers["Cache-Control"] = "private, max-age=3600"
+            if file_object["extname"] not in config["cache"]["ignore"]:
+                self.response.headers["Cache-Control"] = config["cache"]["header"]
             else:
-                # Do not save on cache
                 self.response.headers["Cache-Control"] = "no-cache"
             # Set the mime-type
             self.response.content_type = file_object["mime_type"]
             # Write the file content and finish the request
             return self.response.write(file_object["content"])
         except:
-            # Render the 404 error page
             logging.error("Error reading file from " + file_object["storage_path"])
             logging.error("Requested path: " + file_object["pathname"])
+            # Render the 404 error page
             return render_static(self, 404, "error.html")
         # Something went wrong
         logging.critical("Internal error reading file from " + file_object["storage_path"])
