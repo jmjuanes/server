ba17159fe26ce91cadbd6d0eb3ed3e5e1cea795a
jmjuanes <josemijuanes@gmail.com>
2018-12-08 13:52:55 +0100

    Init templates build script

---
+++ scripts/build.js
@@ -0,0 +1,37 @@
+//Import dependencies
+let fs = require("fs");
+let path = require("path");
+let handlebars = require("handlebars");
+
+//Import configuration file
+let config = require(path.join(process.cwd(), "config.json"));
+
+//Build templates
+process.nextTick(function () {
+    //Load template file
+    let templatePath = path.join(process.cwd(), "templates", "index.html");
+    let templateContent = fs.readFileSync(templatePath, "utf8");
+    //Generate the handlebars template
+    let template = handlebars.compile(templateContent);
+    //Output static files path
+    let staticFolder = path.join(process.cwd(), "static");
+    //Chekc if the static folder exists
+    if (fs.existsSync(staticFolder) === false) {
+        fs.mkdirSync(staticFolder);
+    }
+    //Build each static file
+    Object.keys(config.staticPages).forEach(function (key) {
+        console.log("Building " + key + " template");
+        //Generate the file content
+        let content = template({
+            "site": config.site,
+            "content": config.staticPages[key]
+        });
+        //Save the static file
+        let staticPath = path.join(staticFolder, key + ".html");
+        fs.writeFileSync(staticPath, content, "utf8");
+    });
+    //Finish build
+    console.log("Build finished");
+});
+
